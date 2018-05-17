2101771dcf55c540d2e9be6eb3035a8845cb0fe1
0phoff <0phoff@users.noreply.github.com>
2018-05-17 11:21:01 +0200

    Added colored logger (originally created for Lightnet)

--- top/__init__.py
+++ top/__init__.py
@@ -3,13 +3,14 @@ TOP Crafts: Random bits and bops that are often useful
 """
 import os as _os
 
-
 # Base imports
 from .util import *
+from ._log import *
 
 # Metadata
 _dir = _os.path.dirname(_os.path.realpath(__file__))
 submodules = {
+    'top.logger': get_module_docstring(_dir + '/_log.py'),
     'top.util': get_module_docstring(_dir + '/util/__init__.py'),
     'top.pd': get_module_docstring(_dir + '/pd/__init__.py'),
     'top.torch': get_module_docstring(_dir + '/torch/__init__.py')
