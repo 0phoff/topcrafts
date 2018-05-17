54f754b86702db71498299e972aba2443da0a6e6
0phoff <0phoff@users.noreply.github.com>
2018-05-17 12:35:25 +0200

    Added latex table function

--- top/util/_latex.py
+++ top/util/_latex.py
@@ -1,12 +1,13 @@
-"""Visualisation stuff"""
+"""LaTeX stuff for publications"""
 import logging
 from math import sqrt
 import matplotlib.pyplot as plt
 
-__all__ = ['latexify', 'format_axes']
+__all__ = ['mpl_latex', 'mpl_latex_axes', 'latex_table']
 log = logging.getLogger(__name__)
 
-def latexify(fig_width=None, fig_height=None, columns=1):
+
+def mpl_latex(fig_width=None, fig_height=None, columns=1):
     """ Set up matplotlib's RC params for LaTeX publication ready plots.
     Code stolen and adapted from: https://nipunbatra.github.io/blog/2014/latexify.html
 
@@ -50,7 +51,7 @@ def latexify(fig_width=None, fig_height=None, columns=1):
     matplotlib.rcParams.update(params)
 
 
-def format_axes(ax):
+def mpl_latex_axes(ax):
     """ Bettter axes for publications.
     Code stolen and adapted from: https://nipunbatra.github.io/blog/2014/latexify.html
 
@@ -70,3 +71,62 @@ def format_axes(ax):
         axis.set_tick_params(direction='out')
 
     return ax
+
+
+def latex_table(data, out_file=None, alignment=None, header=None):
+    """ Creates a LaTeX tabular table from your data.
+
+    Args:
+        data (2D list or pandas.DataFrame): data for table
+        out_file (str, optional): Output filename; Default **no file created**
+        alignment (str, optional): Tabular alignment string; Default **left align all columns with lines in between**
+        header (list, optional): Header to be inserted before data; Default **Bold DataFrame column names or nothing**
+
+    Returns:
+        str: LaTeX tabular table as a list of strings
+
+    Example:
+       You should include the resulting .tex file in the following way
+
+       .. highlight:: tex
+          
+          \\begin{table}
+            \\centering
+            \\input{generated_latex_file.tex}
+            \\caption{Example of a generated tex table}
+            \\label{tab:generated_table}
+          \\end{table}
+    """
+    # Parse Arguments
+    if alignment is None:
+        if isinstance(data, list):
+            alignment = '|' + 'l|' * len(data[0])
+        else:
+            alignment = '|' + 'l|' * len(data.columns)
+
+    if header is None and not isinstance(data, list):
+        header = list(data.columns)
+        header = ['\\textbf{'+head+'}' for head in header]
+
+    # Create table
+    latex_str = ['\\begin{tabular}{'+alignment+'}']
+    if header is not None:
+        latex_str.append(' & '.join(header) + ' \\\\')
+        latex_str.append('\hline')
+
+    if isinstance(data, list):
+        for d in data:
+            latex_str.append(' & '.join([str(item) for item in d]) + ' \\\\')
+    else:
+        for row in data.iterrows():
+            d = list(row[1])
+            latex_str.append(' & '.join([str(item) for item in d]) + ' \\\\')
+
+    latex_str.append('\\end{tabular}')
+
+    # Write file
+    if out_file is not None:
+        with open(out_file, 'w') as fp:
+            fp.write('\n'.join(latex_str))
+
+    return latex_str
