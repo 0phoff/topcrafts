b800e0266809d9403f59d13f497a42bf39410dca
0phoff <0phoff@users.noreply.github.com>
2018-08-13 16:46:36 +0200

    Added pytorch feature map visualisation function

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-import setuptools as setup
 import os
+import setuptools as setup
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
 
 def find_packages():
     return ['top'] + ['top.'+p for p in setup.find_packages('top')]
@@ -18,17 +26,22 @@ def create_version():
     return version
 
 
+requirements = [
+    'numpy',
+    'scipy',
+    'matplotlib',
+]
+pillow_req = 'pillow-simd' if get_dist('pillow-simd') is not None else 'pillow'
+requirements.append(pillow_req)
+
 version = create_version()
-setup.setup(name='top',
-            version=version,
-            author='0phoff',
-            description='TOPCraft: Bits and bops that I often need and dont want to rewrite',
-            long_description=open('README.md').read(),
-            packages=find_packages(),
-            scripts=find_scripts(),
-            install_requires=[
-                'numpy',
-                'scipy',
-                'matplotlib',
-            ],
+setup.setup(
+    name='top',
+    version=version,
+    author='0phoff',
+    description='TOPCraft: Bits and bops that I often need and dont want to rewrite',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    scripts=find_scripts(),
+    install_requires=requirements,
 )
--- top/torch/_visualisation.py
+++ top/torch/_visualisation.py
@@ -1,9 +1,15 @@
 """ Visualisation of PyTorch stuff """
+import math
+import logging
+import functools
+from pathlib import Path
 import torch
 import numpy as np
+from PIL import Image
 import matplotlib.pyplot as plt
 
-__all__ = ['show_tensor', 'show_tensor_freq', 'show_weights']
+__all__ = ['show_tensor', 'show_tensor_freq', 'show_weights', 'get_activations']
+log = logging.getLogger(__name__)
 
 
 def show_tensor(tensor, ax=plt):
@@ -124,3 +130,101 @@ def show_weights(weights, normalize=False, orientation='portrait'):
             show_tensor(weights[i][j], axes[i][j])
 
     return fig
+
+
+def get_activations(modules, return_output=True, save_folder=None, grid=False, normalize=False, border_fill=255):
+    """ Save activation maps of underlying layer operations by registering a forward hook on the different modules.
+    This function can save the feature maps as an n x n image grid and can also return the feature maps as numpy arrays.
+
+    Args:
+        modules (iterable): The modules for which you want to save the output feature map 
+        return_output (Boolean, optional): Save the feature maps in a dictionary that will be returned from this function; Default **True**
+        save_folder (str, optional): Folder to save the feature maps as images; Default **Do not save images**
+        grid (Boolean, optional): Stack the different feature maps in a square grid; Default **False**
+        normalize (Boolean, optional): Normalize the feature maps between 0-255; Default **False**
+        border_fill (int, optional): Fill value between the feature maps when constructing the grid; Default **255**
+
+    Returns:
+        (tuple): [list of hook_handles], output_dict
+
+    Warning:
+        The returned output dict will be empty at first, but will fill only up once you run the network!
+
+    Note:
+        This function will only save the feature maps of the first item in the batch.
+
+    Note:
+        The `modules` iterable can return **name, modules** tuples as well.
+        In that case the name will be used as key for the output dict and in the filename of the saved images.
+        Otherwise an increasing number is used.
+
+    Note:
+        When saving the feature maps as images, they will be always normalized and saved as a grid.
+        This will not impact your argument flags, when returning the maps as an output as well!
+    """
+    def save_activations(name, mod, inp, out):
+        if out.dim() != 4:
+            log.error(f'Output tensor of {name} does not have 4 dimensions [{out.shape}]')
+            return
+    
+        activation = out.detach().cpu().clone().numpy()[0]
+        
+        # Normalize to 0-255
+        if normalize:
+            min_val = activation.min()
+            activation -= min_val
+            max_val = activation.max()
+            activation *= 255 / max_val
+            activation = activation.astype(np.uint8)
+    
+        # Create grid of tensor
+        if grid or save_folder is not None:
+            if not normalize:
+                min_val = activation.min()
+                activation -= min_val
+                max_val = activation.max()
+                tmp_act = (activation * (255 / max_val)).astype(np.uint8)
+            else:
+                tmp_act = activation
+
+            grid_size = math.ceil(math.sqrt(tmp_act.shape[0]))
+            h, w = tmp_act.shape[1:]
+            img = np.ones([grid_size, grid_size, h+2, w+2], dtype=np.uint8) * border_fill
+    
+            for i in range(tmp_act.shape[0]):
+                x_grid = i % grid_size
+                y_grid = i // grid_size
+                img[y_grid, x_grid, 1:h+1, 1:w+1] = tmp_act[i]
+            img = np.reshape(img.transpose((0,2,1,3)), [grid_size*(h+2), grid_size*(w+2)])
+
+            if grid:
+                activation = img
+    
+        # Save activation
+        if save_folder is not None:
+            path = Path(save_folder) / (name + '.png')
+            img = Image.fromarray(img)
+            img.save(str(path))
+
+        if output is not None:
+            output[name] = activation
+
+    if return_output:
+        output = {}
+    else:
+        output = None
+
+    if save_folder is not None:
+        f = Path(save_folder)
+        f.mkdir(parents=True, exist_ok=True)
+
+    ret_handles = []
+    for i, mod in enumerate(modules):
+        if isinstance(mod, tuple):
+            name, mod = mod
+        else:
+            name = str(i)
+
+        ret_handles.append(mod.register_forward_hook(functools.partial(save_activations, name)))
+
+    return ret_handles, output
